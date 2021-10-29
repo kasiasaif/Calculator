a9944686a333e674f4610c2255a776fa4401b3fc
kasiasaif <kasia.saif@gmail.com>
2021-10-29 21:45:28 +0100

    css changes

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,35 +4,37 @@ import "../App.css";
 const Buttons = ({ onClick }) => {
   return (
     <div>
-      <div  className = "buttons" >
-        <button value="+" onClick={(val) => {onClick(val.target.value)}}>{" "}+{" "}</button>
-        <button value="*" onClick={(val) => {onClick(val.target.value)}}>{" "}*{" "}</button>
-        <button value="/" onClick={(val) => {onClick(val.target.value)}}>{" "}/{" "}</button>
+      <div  className = "buttons1" >
+        <button className="btn" value="+" onClick={(val) => {onClick(val.target.value)}}>{" "}+{" "}</button>
+        <button className="btn" value="*" onClick={(val) => {onClick(val.target.value)}}>{" "}*{" "}</button>
+        <button className="btn" value="/" onClick={(val) => {onClick(val.target.value)}}>{" "}/{" "}</button>
       </div> 
-      <div className = "buttons">
-      <button value="%" onClick={(val) => {onClick(val.target.value)}}>{" "}%{" "}</button>
-        <button value="-" onClick={(val) => { onClick(val.target.value)}}>{" "}-{" "}</button>
-        <button value="=" onClick={(val) => { onClick(val.target.value)}}>{" "}={" "}</button>
+      <div className = "buttons1">
+      <button  className="btn" value="%" onClick={(val) => {onClick(val.target.value)}}>{" "}%{" "}</button>
+        <button  className="btn" value="-" onClick={(val) => { onClick(val.target.value)}}>{" "}-{" "}</button>
+        <button  className="btn" id="q" value="=" onClick={(val) => { onClick(val.target.value)}}>{" "}={" "}</button>
       </div>
-      <div className = "buttons">
+      
+      <div className = "buttons1">
+        <button className="button"value="1" onClick={(val) => {onClick(val.target.value)}}>{" "}1{" "}</button>
+        <button  className="button" value="2" onClick={(val) => {onClick(val.target.value)}}>{" "}2{" "}</button>
+        <button  className="button"value="3" onClick={(val) => {onClick(val.target.value)}}>{" "}3{" "}</button>
       </div>
-      <div className = "buttons">
-        <button value="1" onClick={(val) => {onClick(val.target.value)}}>{" "}1{" "}</button>
-        <button value="2" onClick={(val) => {onClick(val.target.value)}}>{" "}2{" "}</button>
-        <button value="3" onClick={(val) => {onClick(val.target.value)}}>{" "}3{" "}</button>
+      <div className = "buttons1">
+      <button  className="button"value="4" onClick={(val) => {onClick(val.target.value)}}>{" "}4{" "}</button>
+      <button  className="button" value="5" onClick={(val) => {onClick(val.target.value)}}>{" "}5{" "}</button>
+      <button  className="button" value="6" onClick={(val) => {onClick(val.target.value)}}>{" "}6{" "}</button>
       </div>
-      <button value="4" onClick={(val) => {onClick(val.target.value)}}>{" "}4{" "}</button>
-      <button value="5" onClick={(val) => {onClick(val.target.value)}}>{" "}5{" "}</button>
-      <button value="6" onClick={(val) => {onClick(val.target.value)}}>{" "}6{" "}</button>
-      <div className = "buttons">
-        <button value="7" onClick={(val) => {onClick(val.target.value)}}>{" "}7{" "}</button>
-        <button value="8" onClick={(val) => { onClick(val.target.value)}}>{" "}8{" "}</button>
-        <button value="9" onClick={(val) => {onClick(val.target.value)}}>{" "}9{" "}</button>
+      <div className = "buttons1">
+        <button  className="button" value="7" onClick={(val) => {onClick(val.target.value)}}>{" "}7{" "}</button>
+        <button  className="button"value="8" onClick={(val) => { onClick(val.target.value)}}>{" "}8{" "}</button>
+        <button  className="button" value="9" onClick={(val) => {onClick(val.target.value)}}>{" "}9{" "}</button>
       </div>
-      <button value="Clear" onClick={(val) => {onClick(val.target.value);}}>{" "}Clear{" "}</button>
-      <button value="0" onClick={(val) => {onClick(val.target.value)}}>{" "}0{" "}</button>
-      <button value="BackSpace" onClick={(val) => {onClick(val.target.value)}}>{" "}Back{" "}</button>
-
+      <div className = "buttons1">
+      <button  className="btn"  id="z" value="Clear" onClick={(val) => {onClick(val.target.value);}}>{" "}Clear{" "}</button>
+      <button className="button" value="0" onClick={(val) => {onClick(val.target.value)}}>{" "}0{" "}</button>
+      <button  className="btn" id="z" value="BackSpace" onClick={(val) => {onClick(val.target.value)}}>{" "}Back{" "}</button>
+      </div>
     </div>
   );
 }
